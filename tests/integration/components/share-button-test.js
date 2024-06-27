4b55ed4f8d436538d025b1bd2c7f46f467ab3c71
Robert Edward Friedel <Robert.Friedel@gft.com>
2024-06-27 18:51:55 +0200

    add: test service mock and some other test covering functionality

--- tests/integration/components/share-button-test.js
+++ tests/integration/components/share-button-test.js
@@ -1,26 +1,92 @@
 import { module, test } from 'qunit';
 import { setupRenderingTest } from 'ember-super-rentals/tests/helpers';
-import { render } from '@ember/test-helpers';
+import { find, render } from '@ember/test-helpers';
 import { hbs } from 'ember-cli-htmlbars';
+import Service from '@ember/service';
+
+const MOCK_URL = new URL(
+  '/foo/bar?baz=true#some-section',
+  window.location.origin,
+);
+
+class MockRouterService extends Service {
+  get currentURL() {
+    return '/foo/bar?baz=true#some-section';
+  }
+}
 
 module('Integration | Component | share-button', function (hooks) {
   setupRenderingTest(hooks);
 
+  hooks.beforeEach(function () {
+    this.owner.register('service:router', MockRouterService);
+
+    this.tweetParam = (param) => {
+      let link = find('a');
+      let url = new URL(link.href);
+      return url.searchParams.get(param);
+    };
+  });
+
   test('it renders', async function (assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
+      // Set any properties with this.set('myProperty', 'value');
+      // Handle any actions with this.set('myAction', function(val) { ... });
+    await render(hbs`<ShareButton>Tweet this!</ShareButton>`);
+    assert
+    .dom('a')
+    .hasAttribute('target', '_blank')
+    .hasAttribute('rel', 'external nofollow noopener noreferrer')
+    .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/)
+    .hasClass('share')
+    .hasClass('button')
+    .containsText('Tweet this!');
 
-    await render(hbs`<ShareButton />`);
+    assert.strictEqual(this.tweetParam('url'), MOCK_URL.href);
+  });
 
-    assert.dom().hasText('');
+  test('it supports passing @text', async function (assert) {
+    await render(
+      hbs`<ShareButton @text="Hello Twitter!">Tweet this!</ShareButton>`,
+    );
 
-    // Template block usage:
-    await render(hbs`
-      <ShareButton>
-        template block text
-      </ShareButton>
-    `);
+    assert.strictEqual(this.tweetParam('text'), 'Hello Twitter!');
+  });
 
-    assert.dom().hasText('template block text');
+  test('it supports passing @hashtags', async function (assert) {
+    await render(
+      hbs`<ShareButton @hashtags="foo,bar,baz">Tweet this!</ShareButton>`,
+    );
+
+    assert.strictEqual(this.tweetParam('hashtags'), 'foo,bar,baz');
+  });
+
+  test('it supports passing @via', async function (assert) {
+    await render(hbs`<ShareButton @via="emberjs">Tweet this!</ShareButton>`);
+    assert.strictEqual(this.tweetParam('via'), 'emberjs');
+  });
+
+  test('it supports adding extra classes', async function (assert) {
+    await render(
+      hbs`<ShareButton class="extra things">Tweet this!</ShareButton>`,
+    );
+
+    assert
+      .dom('a')
+      .hasClass('share')
+      .hasClass('button')
+      .hasClass('extra')
+      .hasClass('things');
+  });
+
+  test('the target, rel and href attributes cannot be overridden', async function (assert) {
+    await render(
+      hbs`<ShareButton target="_self" rel="" href="/">Not a Tweet!</ShareButton>`,
+    );
+
+    assert
+      .dom('a')
+      .hasAttribute('target', '_blank')
+      .hasAttribute('rel', 'external nofollow noopener noreferrer')
+      .hasAttribute('href', /^https:\/\/twitter\.com\/intent\/tweet/);
   });
 });
